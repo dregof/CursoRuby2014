410537949ae7f3e377ee4b3fc9edf398e0fedc2b
Pablo Formoso Estrada <pablo@pabloformoso.com>
2014-04-11 11:26:22 +0200

    11

---
+++ book_shop/app/controllers/api/products_controller.rb
@@ -0,0 +1,11 @@
+module Api
+  class ProductsController < Api::BaseController
+    def index
+      render text: "Hola"
+    end
+
+    def show
+      render text: "Hola"
+    end
+  end
+end
--- book_shop/config/routes.rb
+++ book_shop/config/routes.rb
@@ -1,14 +1,15 @@
 BookShop::Application.routes.draw do
 
+  root 'welcome#index'
+
   get "cart/add_product/:product_id" => "cart#add_product", as: :add_product
   get "cart/remove_product/:product_id" => "cart#remove_product", as: :remove_product
+  get "/categories/:key" => "welcome#index", as: :categories
 
   post "search" => "welcome#index", as: :search
 
   devise_for :administrators
   devise_for :users
-
-  get "/categories/:key" => "welcome#index", as: :categories
 
   namespace :admin do
     root "dashboard#index"
@@ -16,51 +17,21 @@ BookShop::Application.routes.draw do
     resources :products
   end
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  root 'welcome#index'
-
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
+  ##
+  # http://api.localhost
   #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+  constraints subdomain: "api" do
+    namespace :api, path: "/" do
+      resources :products, only: [:index, :show]
+    end
+  end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
 
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
 
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
+
+
+
+
+
+
 end
